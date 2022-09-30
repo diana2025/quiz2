55a88fa92fda307febc8fe4c33505e020b00f58f
Sherif <sherifmagdy@gmail.com>
2022-09-30 15:35:49 +0200

    Iteration

---
+++ venv/quiz.py
@@ -0,0 +1,15 @@
+userAnswer1 = input("Vilken funktion används för att få input från användaren?")
+correctAnswer1 = "input"
+if correctAnswer1 == userAnswer1:
+    print("Rätt")
+if correctAnswer1 != userAnswer1:
+    print("Fel: rätt svar är: " + correctAnswer1)
+
+
+userAnswer1 = input("Vilken operator används för att addera 2 siffror?")
+correctAnswer1 = "+"
+if correctAnswer1 == userAnswer1:
+    print("Rätt")
+if correctAnswer1 != userAnswer1:
+    print("Fel: rätt svar är: " + correctAnswer1)
+
